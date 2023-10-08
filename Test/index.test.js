ed79ea725cd63449a04201ef1999520c49230c0d
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-08 20:10:08 -0300

    Finalizing project

---
+++ Test/index.test.js
@@ -0,0 +1,5 @@
+const index = require('../index.js');
+
+test('Ensures we have our index.', () => {
+    expect(index).not.toBeFalsy();
+})
---
+++ Test/shapes.test.js
@@ -0,0 +1,6 @@
+const shape = require('../Assets/shapes.js');
+
+test('Properly defines a symbol', () => {
+    expect(shape).not.toBeFalsy();
+})
+// Checks to ensure a shape has been made.
